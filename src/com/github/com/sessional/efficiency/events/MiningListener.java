09c829de6ce37ea7642decaac7c124dbe8d3fc8e
Sessional <Andrew@Asus-Laptop.(none)>
2012-04-04 22:51:54 -0500

    Some basic starting files

---
+++ src/com/github/com/sessional/efficiency/events/MiningListener.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.com.sessional.efficiency.events;
+
+import com.github.com.sessional.efficiency.EfficiencyPlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+/**
+ *
+ * @author Andrew
+ */
+public class MiningListener implements Listener
+{
+    private EfficiencyPlugin plugin;
+    
+    public MiningListener(EfficiencyPlugin plugin)
+    {
+        this.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event)
+    {
+        
+    }
+}
---
+++ src/com/github/com/sessional/efficiency/events/PlayerSetupListener.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.com.sessional.efficiency.events;
+
+import com.github.com.sessional.efficiency.EfficiencyPlugin;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ *
+ * @author Andrew
+ */
+public class PlayerSetupListener implements Listener
+{
+    private EfficiencyPlugin plugin;
+    
+    public PlayerSetupListener(EfficiencyPlugin plugin)
+    {
+        this.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onPlayerLogin(PlayerJoinEvent event)
+    {
+        
+    }
+}
