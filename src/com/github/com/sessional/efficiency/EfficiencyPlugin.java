4f26a8e551bf7d2adc83e943d112062d65318e64
Sessional <Andrew@Asus-Laptop.(none)>
2012-04-04 22:14:43 -0500

    Commit starting files. Just planning done so far.

---
+++ src/com/github/com/sessional/efficiency/EfficiencyPlugin.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.com.sessional.efficiency;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Andrew
+ */
+public class EfficiencyPlugin extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        
+    }
+    
+    @Override
+    public void onDisable()
+    {
+        
+    }
+}
